a6fd7ea7154183b43fca1b9e91bc2f1894f67475
Hector Simpson <hector@me.com>
2025-05-05 16:52:35 +0200

    Initial commit

--- src/colors.js
+++ src/colors.js
@@ -1,30 +1,15 @@
-const lightColors = require("@primer/primitives/dist/json/colors/light.json");
-const lightHighContrastColors = require("@primer/primitives/dist/json/colors/light_high_contrast.json");
-const lightColorblindColors = require("@primer/primitives/dist/json/colors/light_colorblind.json");
-const darkColors = require("@primer/primitives/dist/json/colors/dark.json");
-const darkHighContrastColors = require("@primer/primitives/dist/json/colors/dark_high_contrast.json");
-const darkColorblindColors = require("@primer/primitives/dist/json/colors/dark_colorblind.json");
-const dimmedColors = require("@primer/primitives/dist/json/colors/dark_dimmed.json");
+const lightColors = require("./json/light.json");
+const darkColors = require("./json/dark.json");
 
 function getColors(theme) {
-
-  switch(theme) {
+  switch (theme) {
     case "light":
-
       // Temp override until Primitives are updated
-      lightColors.success.emphasis = "#1f883d";
       lightColors.btn.primary.bg = lightColors.success.emphasis;
       lightColors.btn.primary.hoverBg = lightColors.scale.green[5];
-      lightColors.fg.default = "#1f2328";
-      lightColors.fg.muted = "#656d76";
 
       return lightColors;
-    case "light_high_contrast":
-      return lightHighContrastColors;
-    case "light_colorblind":
-        return lightColorblindColors;
     case "dark":
-
       // Temp override until Primitives are updated
       darkColors.fg.default = "#e6edf3";
       darkColors.fg.muted = "#7d8590";
@@ -35,12 +20,6 @@ function getColors(theme) {
       darkColors.sponsors.subtle = "rgba(219, 97, 162, 0.1)";
 
       return darkColors;
-    case "dark_high_contrast":
-      return darkHighContrastColors;
-    case "dark_colorblind":
-      return darkColorblindColors;
-    case "dark_dimmed":
-      return dimmedColors;
     default:
       throw new Error(`Colors are missing for value: ${theme}`);
   }
